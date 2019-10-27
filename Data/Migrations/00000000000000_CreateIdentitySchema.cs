84c7e858c2b811c443b399ab2e1b82748af61609
Simen Eilevstjønn <simene@lyse.net>
2019-10-27 09:39:38 +0100

    Update migration

--- Data/Migrations/00000000000000_CreateIdentitySchema.cs
+++ Data/Migrations/00000000000000_CreateIdentitySchema.cs
@@ -40,7 +40,8 @@ namespace FerieCountdownWithAuth.Data.Migrations
                     TwoFactorEnabled = table.Column<bool>(nullable: false),
                     LockoutEnd = table.Column<DateTimeOffset>(nullable: true),
                     LockoutEnabled = table.Column<bool>(nullable: false),
-                    AccessFailedCount = table.Column<int>(nullable: false)
+                    AccessFailedCount = table.Column<int>(nullable: false),
+                    PrefferedLocale = table.Column<int>(nullable: true)
                 },
                 constraints: table =>
                 {
