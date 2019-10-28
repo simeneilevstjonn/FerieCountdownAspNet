d334bdf14858ecfb980d2058082e7a53891f9bd5
Simen Eilevstjønn <simene@lyse.net>
2019-10-28 17:12:10 +0100

    Time locale update

--- Controllers/SettingsController.cs
+++ Controllers/SettingsController.cs
@@ -21,22 +21,22 @@ namespace FerieCountdownWithAuth.Controllers
 
         public IActionResult Jsongenerator()
         {
-            /* 
+             
             //LUNDEHAUGEN
-            CountdownLocaleData locale = new CountdownLocaleData
+            /*CountdownLocaleData locale = new CountdownLocaleData
             {
                 MondayEnd = new Time(14, 20),
                 TuesdayEnd = new Time(13, 15),
                 WednesdayEnd = new Time(14, 20),
                 ThursdayEnd = new Time(14, 20),
                 FridayEnd = new Time(13, 15),
-                AutumnHoliday = DateTime.Parse("2019-10-04T11:15:00Z", null, System.Globalization.DateTimeStyles.RoundtripKind),
+                AutumnHoliday = DateTime.Parse("2020-10-02T11:15:00Z", null, System.Globalization.DateTimeStyles.RoundtripKind),
                 ChristmasHoliday = DateTime.Parse("2019-12-21T09:25:00Z", null, System.Globalization.DateTimeStyles.RoundtripKind),
                 WinterHoliday = DateTime.Parse("2020-02-21T12:15:00Z", null, System.Globalization.DateTimeStyles.RoundtripKind),
                 EasterHoliday = DateTime.Parse("2020-04-03T11:15:00Z", null, System.Globalization.DateTimeStyles.RoundtripKind),
                 SummerHoliday = DateTime.Parse("2020-06-19T08:25:00Z", null, System.Globalization.DateTimeStyles.RoundtripKind)
-            };
-            */
+            };*/
+            
 
             /*
             //SKEIANE
@@ -55,10 +55,10 @@ namespace FerieCountdownWithAuth.Controllers
             };
             */
 
-            /*
-            ViewData["json"] = JsonConvert.SerializeObject(locale);
-            return View();
-            */
+            
+            /*ViewData["json"] = JsonConvert.SerializeObject(locale);
+            return View();*/
+            
 
             return Forbid();
         }
