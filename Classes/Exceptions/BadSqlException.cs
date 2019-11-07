ba80f504605ebe88798488eb62f5011d7b144fbe
Simen Eilevstjønn <simene@lyse.net>
2019-11-07 17:08:34 +0100

    Add custom exceptions

---
+++ Classes/Exceptions/BadSqlException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FerieCountdown.Classes.Exceptions
+{
+    public class BadSqlException : Exception
+    {
+        public BadSqlException()
+        {
+        }
+
+        public BadSqlException(string message)
+            : base(message)
+        {
+        }
+
+        public BadSqlException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
