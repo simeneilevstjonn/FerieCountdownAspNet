c7b67d8c8ca3a53725022603b05c2a11a0db75fb
Simen Eilevstjønn <simene@lyse.net>
2019-11-14 07:56:00 +0100

    Copyright info

---
+++ Classes/CopyrightItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FerieCountdown.Classes
+{
+    public class CopyrightItem
+    {
+        public string Name { get; set; }
+        public string Liscense { get; set; }
+        public string CopyrightHolder { get; set; }
+        public string Link { get; set; }
+        public string Image { get; set; }
+
+        public CopyrightItem(string Name, string Liscense, string CopyrightHolder, string Link, string Image)
+        {
+            this.Name = Name;
+            this.Liscense = Liscense;
+            this.CopyrightHolder = CopyrightHolder;
+            this.Link = Link;
+            this.Image = Image;
+        }
+    }
+}
