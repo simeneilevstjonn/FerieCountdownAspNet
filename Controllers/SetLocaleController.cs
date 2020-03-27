867f7c9d0f2d46dd4f9ffa0335652ffb3d66425f
Simen Eilevstjønn <simene@lyse.net>
2020-03-27 16:59:10 +0100

    Remove custom redirect after submit

--- Controllers/SetLocaleController.cs
+++ Controllers/SetLocaleController.cs
@@ -48,8 +48,7 @@ namespace FerieCountdown.Controllers
 
             DbMaster.SqlQuery(string.Format("DELETE FROM [dbo].[CustomLocales] WHERE UserId = N'{0}'; INSERT INTO [dbo].[CustomLocales] ([UserId], [Data], [IsWork]) VALUES (N'{0}', N'{1}', {2});", User.FindFirstValue(ClaimTypes.NameIdentifier), JsonConvert.SerializeObject(cld), IsWork));
 
-            if (Request.Query["redirecturi"].ToString() == null) return Redirect("/");
-            else return Redirect(Request.Form["RedirectUri"]);
+            return Redirect("/");
         }
 
     }
