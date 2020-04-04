e2983a1291a66692b4ff5052ff3a0ea9acd0224a
Simen Eilevstjønn <simene@lyse.net>
2020-04-04 11:13:04 +0200

    New strings to avoid nullreferenceexception

--- Classes/Io/ContactFormEmail.cs
+++ Classes/Io/ContactFormEmail.cs
@@ -8,10 +8,13 @@ namespace FerieCountdown.Classes.Io
 {
     public class ContactFormEmail : EmailTemplate
     {
-        public ContactFormEmail()
+        public new string Body 
         {
-            Body = new string(string.Format("<table> <tr> <td>Fra</td><td>{0}, {1}</td></tr><tr> <td>Sendt</td><td>{2}</td></tr><tr> <td colspan=\"2\"> Melding </td></tr><tr> <td colspan=\"2\">{3}</td></tr></table>",ReplyTo.DisplayName, ReplyTo.Address, DateTime.UtcNow.ToString("u"), Message));
-            RightFooterData = new Dictionary<string, string> 
+            get => string.Format("<table> <tr> <td>Fra</td><td>{0}, {1}</td></tr><tr> <td>Sendt</td><td>{2}</td></tr><tr> <td colspan=\"2\"> Melding </td></tr><tr> <td colspan=\"2\">{3}</td></tr></table>", ReplyTo.DisplayName, ReplyTo.Address, DateTime.UtcNow.ToString("u"), Message);
+        }
+        public new Dictionary<string, string> RightFooterData
+        {
+            get => new Dictionary<string, string>
             {
                 { "Bruker-IP",UserIP},
                 { "Land",UserCountryCode}
