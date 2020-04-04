2a41a344e60ca94aee22a2582444c3a1aa3ebba0
Simen Eilevstjønn <simene@lyse.net>
2020-04-04 10:57:31 +0200

    Trying to fix a nullreferenceexception

--- Classes/Io/ContactFormEmail.cs
+++ Classes/Io/ContactFormEmail.cs
@@ -10,9 +10,7 @@ namespace FerieCountdown.Classes.Io
     {
         public ContactFormEmail()
         {
-            Body = string.Format("<table> <tr> <td>Fra</td><td>{0}, {1}</td></tr><tr> <td>Sendt</td><td>{2}</td></tr><tr> <td colspan=\"2\"> Melding </td></tr><tr> <td colspan=\"2\">{3}</td></tr></table>",
-                ReplyTo.DisplayName, ReplyTo.Address, DateTime.UtcNow.ToString("u"), Message);
-
+            Body = new string(string.Format("<table> <tr> <td>Fra</td><td>{0}, {1}</td></tr><tr> <td>Sendt</td><td>{2}</td></tr><tr> <td colspan=\"2\"> Melding </td></tr><tr> <td colspan=\"2\">{3}</td></tr></table>",ReplyTo.DisplayName, ReplyTo.Address, DateTime.UtcNow.ToString("u"), Message));
             RightFooterData = new Dictionary<string, string> 
             {
                 { "Bruker-IP",UserIP},
