50f8128281d1d47085c56189e2da4d2844b527c4
Simen Eilevstjønn <simene@lyse.net>
2021-01-08 19:07:00 +0100

    #80 Add red tier data storage

--- Classes/Locale/CountdownLocale.cs
+++ Classes/Locale/CountdownLocale.cs
@@ -13,7 +13,67 @@ namespace FerieCountdown.Classes.Locale
         public string School { get; set; }
         public CountdownLocaleData LocaleData { get; set; }
         public bool IsWork { get; set; }
-        public string Data { set
+        public bool UseRedTier { get; set; }
+        public List<RedTierData> CohortData { get; private set; }
+        public string RedTierData
+        {
+            set
+            {
+                // Parse JSON
+                JObject Data = JObject.Parse(value);
+
+                // Initialize CohortData
+                CohortData = new List<RedTierData>();
+
+                // Repeat for each cohort in array
+                for (int i = 0; i < Data.Count; i++)
+                {
+                    // Define an instance of RedTierData
+                    RedTierData rtd = new RedTierData();
+
+                    // Check if Always exist within Cohort
+                    if (Data[i]["Always"] != null)
+                    {
+                        // Parse Always
+                        rtd.Always = new CountdownLocaleData
+                        {
+                            MondayEnd = new Time((int)Data[i]["Always"]["MondayEnd"]["Hours"], (int)Data[i]["Always"]["MondayEnd"]["Minutes"]),
+                            TuesdayEnd = new Time((int)Data[i]["Always"]["TuesdayEnd"]["Hours"], (int)Data[i]["Always"]["TuesdayEnd"]["Minutes"]),
+                            WednesdayEnd = new Time((int)Data[i]["Always"]["WednesdayEnd"]["Hours"], (int)Data[i]["Always"]["WednesdayEnd"]["Minutes"]),
+                            ThursdayEnd = new Time((int)Data[i]["Always"]["ThursdayEnd"]["Hours"], (int)Data[i]["Always"]["ThursdayEnd"]["Minutes"]),
+                            FridayEnd = new Time((int)Data[i]["Always"]["FridayEnd"]["Hours"], (int)Data[i]["Always"]["FridayEnd"]["Minutes"]),
+                        };
+                    }
+                    else
+                    {
+                        // Parse OddWeek and EvenWeek
+                        rtd.OddWeek = new CountdownLocaleData
+                        {
+                            MondayEnd = new Time((int)Data[i]["OddWeek"]["MondayEnd"]["Hours"], (int)Data[i]["OddWeek"]["MondayEnd"]["Minutes"]),
+                            TuesdayEnd = new Time((int)Data[i]["OddWeek"]["TuesdayEnd"]["Hours"], (int)Data[i]["OddWeek"]["TuesdayEnd"]["Minutes"]),
+                            WednesdayEnd = new Time((int)Data[i]["OddWeek"]["WednesdayEnd"]["Hours"], (int)Data[i]["OddWeek"]["WednesdayEnd"]["Minutes"]),
+                            ThursdayEnd = new Time((int)Data[i]["OddWeek"]["ThursdayEnd"]["Hours"], (int)Data[i]["OddWeek"]["ThursdayEnd"]["Minutes"]),
+                            FridayEnd = new Time((int)Data[i]["OddWeek"]["FridayEnd"]["Hours"], (int)Data[i]["OddWeek"]["FridayEnd"]["Minutes"]),
+                        };
+
+                        rtd.EvenWeek = new CountdownLocaleData
+                        {
+                            MondayEnd = new Time((int)Data[i]["EvenWeek"]["MondayEnd"]["Hours"], (int)Data[i]["EvenWeek"]["MondayEnd"]["Minutes"]),
+                            TuesdayEnd = new Time((int)Data[i]["EvenWeek"]["TuesdayEnd"]["Hours"], (int)Data[i]["EvenWeek"]["TuesdayEnd"]["Minutes"]),
+                            WednesdayEnd = new Time((int)Data[i]["EvenWeek"]["WednesdayEnd"]["Hours"], (int)Data[i]["EvenWeek"]["WednesdayEnd"]["Minutes"]),
+                            ThursdayEnd = new Time((int)Data[i]["EvenWeek"]["ThursdayEnd"]["Hours"], (int)Data[i]["EvenWeek"]["ThursdayEnd"]["Minutes"]),
+                            FridayEnd = new Time((int)Data[i]["EvenWeek"]["FridayEnd"]["Hours"], (int)Data[i]["EvenWeek"]["FridayEnd"]["Minutes"]),
+                        };
+                    }
+
+                    // Add rtd to lsit
+                    CohortData.Add(rtd);
+                }
+            }
+        }
+        public string Data 
+        { 
+            set
             {
                 JObject ObjectData = JObject.Parse(value);
                 try
