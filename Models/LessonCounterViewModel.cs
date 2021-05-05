28096cd703d511c7de5f555176a640be500f5778
Simen Eilevstjønn <simene@lyse.net>
2021-05-05 22:28:08 +0200

    Remaining lesson counter

---
+++ Models/LessonCounterViewModel.cs
@@ -0,0 +1,31 @@
+﻿using FerieCountdown.Classes.LessonCounter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static FerieCountdown.Classes.LessonCounter.Subjects;
+
+namespace FerieCountdown.Models
+{
+    public class LessonCounterViewModel
+    {
+        public Dictionary<Subject, int> RemainingCount { get; set; }
+        public Dictionary<string, int> RemainingCountStrings
+        {
+            get
+            {
+                // Define the return dict
+                Dictionary<string, int> Count = new Dictionary<string, int>();
+
+                // Iterate through dict
+                foreach ((Subject s, int c) in RemainingCount)
+                {
+                    Count.Add(ToNbNoString(s), c);
+                }
+
+                // Return dict
+                return Count;
+            }
+        }
+    }
+}
